d3f5ca3a8754a6c2b2ab2f3db72e5c03ec7c1ef6
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-06-03 10:42:23 -0700

    Post routes complete

--- models/index.js
+++ models/index.js
@@ -2,5 +2,16 @@ const User = require('./User');
 const Post = require('./Post');
 
 
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+// need to define the reverse of the prior association definition
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+})
+
+
 
 module.exports = { User, Post };
