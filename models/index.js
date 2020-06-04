b3efea02821b03b3bb1c105452ae7bd91e522974
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-06-04 13:57:07 -0700

    begin Comment model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 
 // create associations
@@ -46,7 +47,25 @@ Post.hasMany(Vote, {
 });
 
 
+// model associations for Comment
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
 
 
 
-module.exports = { User, Post, Vote };
+
+
+module.exports = { User, Post, Vote, Comment };
