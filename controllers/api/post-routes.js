21489daace90cd33c2d45a8c68d8d3f116ec9ed4
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-06-10 15:19:33 -0700

    using helpers to post comments and upvote with conditional templates

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -97,19 +97,33 @@ router.post('/', (req, res) => {
 });
 
 
+router.put('/upvote', (req, res) => {
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
+});
+
+/* final code for module13, but changing for module14 to support sessions */
 // PUT /api/posts/upvote
 // we set the Vote apis in Post bc the associations we created
 // involves two queries: first, using the Vote model to create a vote, then querying on that post to get an updated vote count
-router.put('/upvote', (req, res) => {
-  // create the vote
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
-});
+// router.put('/upvote', (req, res) => {
+//   // create the vote
+//   // custom static method created in models/Post.js
+//   Post.upvote(req.body, { Vote })
+//     .then(updatedPostData => res.json(updatedPostData))
+//     .catch(err => {
+//       console.log(err);
+//       res.status(400).json(err);
+//     });
+// });
   
   // original code prior to refactor
 //   Vote.create({
